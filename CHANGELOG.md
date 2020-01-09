651b39460a3db686390f8c74461690f28b99aeae
Hugo Parente Lima <hugo.pl@gmail.com>
2020-01-09 09:50:34 -0300

    Add simple text alignment to Label widget, simple because it only works on single line labels.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
 ## Unreleased
+### Added
+- Added simple text alignment to Label widget, simple because it only works on single line labels.
--- src/textui/label.cr
+++ src/textui/label.cr
@@ -1,6 +1,14 @@
 module TextUi
+  enum Alignment
+    Left
+    Center
+    Right
+  end
+
   class Label < Widget
     property text
+
+    property alignment = Alignment::Left
 
     def initialize(parent, @text : String = "")
       super(parent)
@@ -16,7 +24,16 @@ module TextUi
 
     def render
       erase
-      print_lines(0, 0, @text)
+
+      # FIXME: This only works when the text has a single line.
+      case @alignment
+      when Alignment::Left
+        print_lines(0, 0, @text)
+      when Alignment::Center
+        print_lines((width - @text.size)//2, 0, @text)
+      when Alignment::Right
+        print_lines(width - @text.size, 0, @text)
+      end
     end
   end
 end
