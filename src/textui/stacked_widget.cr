200f7e5068a888d850aaf63a4a295192fdd1dc66
Hugo Parente Lima <hugo.pl@gmail.com>
2020-01-22 18:39:37 -0300

    Add StackedWidget#remove(widget)

--- src/textui/stacked_widget.cr
+++ src/textui/stacked_widget.cr
@@ -7,10 +7,10 @@ module TextUi
     end
 
     def current_index=(index : Int32)
-      return if index == @current_index
+      return if index == @current_index || index < 0 || index >= children.size
 
-      @children[index].visible = true
-      @children[@current_index].visible = false
+      children[index].visible = true
+      children[@current_index]?.try(&.visible = false)
       @current_index = index
       current_widget.invalidate
     end
@@ -21,12 +21,34 @@ module TextUi
     end
 
     def current_widget
-      @children[@current_index]
+      widget = children[@current_index]?
+      return widget unless widget.nil?
+
+      cycle
+      current_widget
+    end
+
+    def remove(widget : Widget) : Widget
+      idx = children.index(widget)
+      return widget if idx.nil?
+
+      old_current = current_widget
+      new_current = if widget == old_current
+                      cycle
+                      current_widget
+                    else
+                      old_current
+                    end
+      children.delete(widget)
+      self.current_widget = new_current # update @current_index
+      widget
     end
 
     def cycle
+      raise "No widgets to cycle!" if children.size.zero?
+
       next_index = @current_index + 1
-      next_index = 0 if next_index >= @children.size
+      next_index = 0 if next_index >= children.size
       self.current_index = next_index
     end
 
